c0ffe987a50c71975d2c6ccdccd90dd90f33c359
André Fiedler <fiedler@gewamusic.com>
2026-07-11 01:30:39 +0200

    chore: repin Gerber evidence boundary

--- scripts/GerberTask1Provenance.mjs
+++ scripts/GerberTask1Provenance.mjs
@@ -4,8 +4,8 @@
 export const GERBER_TASK1_PROVENANCE = Object.freeze({
     sourceCommit: '11ba9df32ce966d6626f99f444909ff6c50d2281',
     sourceTree: '1b7813598247b9ec3907a9589aefe084e4a448bd',
-    evidenceCommit: 'bf0bd2445bdfdfe7a31ace44572fe548aaabde87',
-    evidenceTree: '3e207a817187624537b9e271fc01b0158daaa816',
-    harnessCommit: 'bf0bd2445bdfdfe7a31ace44572fe548aaabde87',
-    harnessTree: '3e207a817187624537b9e271fc01b0158daaa816'
+    evidenceCommit: '43ab4c2c9abdd14cb4c4ca7e7abb7dd07d7dd021',
+    evidenceTree: 'c1cc89b8d4340c9e48c505e4598b159812ce4ef0',
+    harnessCommit: '43ab4c2c9abdd14cb4c4ca7e7abb7dd07d7dd021',
+    harnessTree: 'c1cc89b8d4340c9e48c505e4598b159812ce4ef0'
 })
