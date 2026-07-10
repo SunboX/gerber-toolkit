3879ef3bf2a3c2ca6dfc1c12d7b1b14b36882584
André Fiedler <fiedler@gewamusic.com>
2026-07-11 01:27:52 +0200

    chore: pin complete Gerber result contracts

--- scripts/GerberTask1Provenance.mjs
+++ scripts/GerberTask1Provenance.mjs
@@ -4,8 +4,8 @@
 export const GERBER_TASK1_PROVENANCE = Object.freeze({
     sourceCommit: '11ba9df32ce966d6626f99f444909ff6c50d2281',
     sourceTree: '1b7813598247b9ec3907a9589aefe084e4a448bd',
-    evidenceCommit: 'f676782b2f26c62694492d800d016c8a82ca2ba8',
-    evidenceTree: 'c50dd92350fdb8c02430ac6a5bb2dc55b70c4a7a',
-    harnessCommit: 'f676782b2f26c62694492d800d016c8a82ca2ba8',
-    harnessTree: 'c50dd92350fdb8c02430ac6a5bb2dc55b70c4a7a'
+    evidenceCommit: 'bf0bd2445bdfdfe7a31ace44572fe548aaabde87',
+    evidenceTree: '3e207a817187624537b9e271fc01b0158daaa816',
+    harnessCommit: 'bf0bd2445bdfdfe7a31ace44572fe548aaabde87',
+    harnessTree: '3e207a817187624537b9e271fc01b0158daaa816'
 })
