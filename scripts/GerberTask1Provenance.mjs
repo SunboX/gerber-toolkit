e3f82c067c39383d69cabfa899f9200bf6e60a13
André Fiedler <fiedler@gewamusic.com>
2026-07-11 02:29:07 +0200

    chore: repin Gerber collection contracts

--- scripts/GerberTask1Provenance.mjs
+++ scripts/GerberTask1Provenance.mjs
@@ -4,8 +4,8 @@
 export const GERBER_TASK1_PROVENANCE = Object.freeze({
     sourceCommit: '11ba9df32ce966d6626f99f444909ff6c50d2281',
     sourceTree: '1b7813598247b9ec3907a9589aefe084e4a448bd',
-    evidenceCommit: '43ab4c2c9abdd14cb4c4ca7e7abb7dd07d7dd021',
-    evidenceTree: 'c1cc89b8d4340c9e48c505e4598b159812ce4ef0',
-    harnessCommit: '43ab4c2c9abdd14cb4c4ca7e7abb7dd07d7dd021',
-    harnessTree: 'c1cc89b8d4340c9e48c505e4598b159812ce4ef0'
+    evidenceCommit: '4590fe5f56042e9dcdfadcf0e29ee3493958da08',
+    evidenceTree: 'fab6213d74f91d15bcd63b0c75d98abe5a0f543b',
+    harnessCommit: '4590fe5f56042e9dcdfadcf0e29ee3493958da08',
+    harnessTree: 'fab6213d74f91d15bcd63b0c75d98abe5a0f543b'
 })
